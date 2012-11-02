e54f69498a350abd90304dc80eec6f03a262f8c9
Marcel Klehr <mklehr@gmx.net>
2012-11-02 14:55:48 +0100

    And God said: 'Let there be light.' And there was light.

---
+++ index.js
@@ -0,0 +1,48 @@
+module.exports = toposort;
+
+function toposort(idProp, ancestryProp, edges) {
+   var nodes = {} // a hash of id=>node
+     , sorted = [] // sorted list of ids
+     , visited = {} // hash: id=>bool
+
+  // list all nodes by id
+  edges.forEach(function(edge) {
+    nodes[getId(edge)] = edge;
+  })
+
+  // topologically sort the nodes by ancestry
+  edges.forEach(function visit(node, ancestors) {
+    if (!Array.isArray(ancestors)) ancestors = []
+    
+    var id = getId(node)
+
+    // if we already visited this, do nothing
+    if (visited[id]) return;
+
+    ancestors.push(id) // ancestor list gets passed to all ancestors
+    visited[id] = true
+
+    getAncestors(node).forEach(function(ancestor) {
+      // if already in ancestors, a cyclic dependency exists.
+      if (ancestors.indexOf(ancestor) >= 0) {
+        throw new Error('Cyclic dependency : ' + ancestor + ' depends on ' + id +', but is a dependency of it.')
+      }
+      
+      // recursively apply 'visit' to all ancestors
+      visit(nodes[ancestor], ancestors.map(function(o){return o}))
+    })
+
+    sorted.unshift(id)
+  })
+
+  return sorted;
+  
+  
+  function getId(obj) {
+    return obj[idProp]
+  }
+  
+  function getAncestors(obj) {
+    return obj[ancestryProp]
+  }
+}
