8838f9898308b12515c2bbfc39ec4f4646f8b638
Sam Marder <sam@marder.co>
2017-10-01 22:52:45 -0400

    Greatly improve sorting performance on large graphs

--- index.js
+++ index.js
@@ -17,35 +17,37 @@ function toposort(nodes, edges) {
     , sorted = new Array(cursor)
     , visited = {}
     , i = cursor
+    // Better data structures make algorithm much faster.
+    , outgoingEdges = makeOutgoingEdges(edges)
+    , nodesHash = makeNodesHash(nodes)
 
   while (i--) {
-    if (!visited[i]) visit(nodes[i], i, [])
+    if (!visited[i]) visit(nodes[i], i, {})
   }
 
   return sorted
 
   function visit(node, i, predecessors) {
-    if(predecessors.indexOf(node) >= 0) {
+    if(predecessors.hasOwnProperty(node)) {
       throw new Error('Cyclic dependency: '+JSON.stringify(node))
     }
 
-    if (!~nodes.indexOf(node)) {
+    if (!nodesHash.hasOwnProperty(node)) {
       throw new Error('Found unknown node. Make sure to provided all involved nodes. Unknown node: '+JSON.stringify(node))
     }
 
     if (visited[i]) return;
     visited[i] = true
 
-    // outgoing edges
-    var outgoing = edges.filter(function(edge){
-      return edge[0] === node
-    })
+    var outgoing = Object.keys(outgoingEdges[node] || {})
+
     if (i = outgoing.length) {
-      var preds = predecessors.concat(node)
+      predecessors[node] = true
       do {
-        var child = outgoing[--i][1]
-        visit(child, nodes.indexOf(child), preds)
+        var child = outgoing[--i]
+        visit(child, nodesHash[child], predecessors)
       } while (i)
+      delete predecessors[node]
     }
 
     sorted[--cursor] = node
@@ -53,11 +55,30 @@ function toposort(nodes, edges) {
 }
 
 function uniqueNodes(arr){
-  var res = []
+  var res = {}
   for (var i = 0, len = arr.length; i < len; i++) {
     var edge = arr[i]
-    if (res.indexOf(edge[0]) < 0) res.push(edge[0])
-    if (res.indexOf(edge[1]) < 0) res.push(edge[1])
+    res[edge[0]] = true
+    res[edge[1]] = true
+  }
+  return Object.keys(res)
+}
+
+function makeOutgoingEdges(arr){
+  var edges = {}
+  for (var i = 0, len = arr.length; i < len; i++) {
+    var edge = arr[i]
+    edges[edge[0]] = edges[edge[0]] || {}
+    edges[edge[1]] = edges[edge[1]] || {}
+    edges[edge[0]][edge[1]] = true
+  }
+  return edges
+}
+
+function makeNodesHash(arr){
+  var res = {}
+  for (var i = 0, len = arr.length; i < len; i++) {
+    res[arr[i]] = i
   }
   return res
 }
